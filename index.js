f0e085a72b8d138b67ec3bb81b583b492b9d4386
Abner--ggabriel <abnergabriel023@hotmail.com>
2024-11-25 20:49:54 -0300

    segundo commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let nome = "Tekido"
+let nivelHeroi = 6787
+let nivel 
+
+if (nivelHeroi <= 1000){
+    nivel = "Ferro"
+    console.log("O Herói de nome "+ nome + " está no nível de " + nivel)
+
+}else if (nivelHeroi >= 1001 && nivelHeroi <= 2000){
+    nivel = "Bronze"
+    console.log("O Herói de nome "+ nome + " está no nível de " + nivel)
+
+}else if (nivelHeroi >= 2001 && nivelHeroi <= 5000){
+    nivel = "Prata"
+    console.log("O Herói de nome "+ nome + " está no nível de " + nivel)
+
+}else if (nivelHeroi >= 5001 && nivelHeroi <= 7000){
+    nivel = "Ouro"
+    console.log("O Herói de nome "+ nome + " está no nível de " + nivel)
+
+}else if (nivelHeroi >= 7001 && nivelHeroi <= 8000){
+    nivel = "Platina"
+    console.log("O Herói de nome "+ nome + " está no nível de " + nivel)
+
+}else if (nivelHeroi >= 8001 && nivelHeroi <= 9000){
+    nivel = "Ascendente"
+    console.log("O Herói de nome "+ nome + " está no nível de " + nivel)
+
+}else if (nivelHeroi >= 9001 && nivelHeroi <= 10000){
+    nivel = "Imortal"
+    console.log("O Herói de nome "+ nome + " está no nível de " + nivel)
+
+}else if (nivelHeroi >= 10001){
+    nivel = "Radiante"
+    console.log("O Herói de nome "+ nome + " está no nível de " + nivel)
+
+}
